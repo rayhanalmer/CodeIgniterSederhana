4c8023900b0e28c665160ac7a6354ff6d3c56c77
unknown <rayhanalmer@gmail.com>
2023-12-01 16:26:13 +0700

    Initial Commit

---
+++ app/Views/HelloWorld.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HelloWorld</title>
+</head>
+<body>
+    style="
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    height: 100vh;
+    background-color: black;
+    "
+>
+    <h1 style="text-align: center; font-size: 10rem; color: white">
+        HelloWorld
+    </h1>
+</body>
+</html>
---
+++ app/Views/welcome_message.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello, World!</title>
+    <style>
+        body {
+            background-color: #282c34;
+            color: #61dafb;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            height: 100vh;
+            margin: 0;
+            font-family: 'Arial', sans-serif;
+        }
+
+        .aesthetic-text {
+            font-size: 3em;
+            letter-spacing: 4px;
+            text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+        }
+    </style>
+</head>
+<body>
+    <div class="aesthetic-text">
+        Hello, World!
+    </div>
+</body>
+</html>
